5e46b393f96e291f86b70469a363968fbd4c428e
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-02-09 15:52:35 +0200

    Добавляет скрипт для открытия и закрытия меню хедера.

--- js/modal.js
+++ js/modal.js
@@ -13,3 +13,18 @@
   }
 })();
 
+
+(() => {
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  //const mobileMenuRef = document.querySelector("[data-menu]");
+
+  menuBtnRef.addEventListener("click", () => {
+    const expanded =
+      menuBtnRef.getAttribute("area-expanded") === "true" || false;
+
+    menuBtnRef.classList.toggle("page-header__menu--is-open");
+    menuBtnRef.setAttribute("area-expanded", !expanded);
+
+    //mobileMenuRef.classList.toggle("is-open");
+  });
+})();
