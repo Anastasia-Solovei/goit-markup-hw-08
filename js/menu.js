5f3d7e3f6dc298eeeb68cac20a7673b05ca12e6a
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-02-14 17:33:00 +0200

    Удаляет класс контейнер у секции, оборачивает контент секции в див с классом контейнер.

--- js/menu.js
+++ js/menu.js
@@ -4,7 +4,7 @@
 
   menuBtnRef.addEventListener("click", () => {
     const expanded =
-      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+     menuBtnRef.getAttribute("aria-expanded") === "true" || false;
 
     menuBtnRef.classList.toggle("page-header__menu--is-open");
     menuBtnRef.setAttribute("aria-expanded", !expanded);
